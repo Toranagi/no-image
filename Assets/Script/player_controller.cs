122be0f0c33c8b2ae129c7757e11c5916025becb
407-12\student <FCA22090022@edu.fca.ac.jp>
2024-11-12 12:25:36 +0900

    2024/11/12 移動まで作った

---
+++ Assets/Script/player_controller.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_contro : MonoBehaviour
+{
+    [SerializeField,Header("ëÃóÕ")]
+    float HP;
+    [SerializeField, Header("çUåÇóÕ")]
+    int STR;
+    [SerializeField, Header("à⁄ìÆë¨ìx")]
+    float speed;
+   
+
+    void InputKey()
+    {
+        var move = new Vector3();
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            move.x-=speed*Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            move.z += speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            move.z -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            move.x += speed * Time.deltaTime;
+        }
+       
+
+        transform.position += move;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputKey();
+    }
+}
